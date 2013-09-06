22b9f674b72393eebf534a571bce01913bc73e43
kviz <kviz@outlook.com>
2013-09-06 08:43:45 +0400

    Добавил SearchService, UsersResult

---
+++ Panzar/Models/UsersResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Panzar.Models
+{
+    public class UsersResult
+    {
+        public Guid Id { get; private set; }
+
+        public bool IsComplete { get; private set; }
+
+        public IEnumerable<User> Result { get; private set; }
+
+        public UsersResult()
+        {
+            Id = Guid.NewGuid();
+            IsComplete = false;
+        }
+
+        public void SetResult(IEnumerable<User> users)
+        {
+            Result = users;
+            IsComplete = true;
+        }
+    }
+}
---
+++ Panzar/Services/SearchService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Panzar.Models;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Panzar.Services
+{
+    public sealed class SearchService
+    {
+        private static readonly object SyncRoot = new Object();
+
+        private static volatile SearchService instance;
+
+        private SearchService()
+        {
+        }
+
+        public static SearchService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (SyncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new SearchService();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public Guid Search(string queryString, HttpSessionStateBase session)
+        {
+            var usersResult = new UsersResult();
+
+            Task.Factory.StartNew(
+                    () =>
+                    {
+                        Thread.Sleep(5000);
+                        var users = new List<User>
+                                        {
+                                            new User { Name = Guid.NewGuid().ToString() }, 
+                                            new User { Name = Guid.NewGuid().ToString() }
+                                        };
+                        usersResult.SetResult(users);
+                    });
+
+
+            session[usersResult.Id.ToString()] = usersResult;
+            return usersResult.Id;
+        }
+    }
+
+}
