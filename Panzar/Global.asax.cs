e7cb590dc024f9307e8f432d6931445651107e9a
kviz <kviz@outlook.com>
2013-09-06 08:59:25 +0400

    Прописал роутинг

--- Panzar/Global.asax.cs
+++ Panzar/Global.asax.cs
@@ -17,6 +17,12 @@ namespace Panzar
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                null,
+                "{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
+
+            routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
